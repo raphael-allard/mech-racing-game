6f9beb3015da3bce3bb0e70259eca76e66a964f4
Raphael Allard <raphael.s.allard@gmail.com>
2024-09-25 21:44:38 +0200

    changed finish line distance

--- javascript/components/moving/sprite_animation.js
+++ javascript/components/moving/sprite_animation.js
@@ -69,7 +69,7 @@ const moveBackSprite = (sprite) => {
 
 // Track how far off we are from the ideal position
 let cumulativeError = 0;
-const threeQuarterFullWidth = window.innerWidth * 0.6;
+const threeQuarterFullWidth = window.innerWidth * 0.7;
 
 const calculatingNextPxMove = () => {
   // Get the total race time in milliseconds
